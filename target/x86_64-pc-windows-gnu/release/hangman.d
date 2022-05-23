2fba6888d69a35910376da84c908175a919d0257
cooscoos <m.perry@strath.ac.uk>
2022-05-23 10:17:20 +0100

    windows compile

---
+++ target/x86_64-pc-windows-gnu/release/hangman.d
@@ -0,0 +1,1 @@
+/app/target/x86_64-pc-windows-gnu/release/hangman.exe: /app/src/lib.rs /app/src/main.rs /app/src/player.rs /app/src/words.rs
---
+++ target/x86_64-pc-windows-gnu/release/libhangman.d
@@ -0,0 +1,1 @@
+/app/target/x86_64-pc-windows-gnu/release/libhangman.rlib: /app/src/lib.rs /app/src/player.rs /app/src/words.rs
